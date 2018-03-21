5560918c001effe8b32db7902db2f5d2326ef03b
dharden15 <daltainhardain15@gmail.com>
2018-03-21 18:46:04 +0000

    final code

--- helloLCD.py
+++ helloLCD.py
@@ -3,6 +3,7 @@
 #
 
 import Adafruit_CharLCD as LCD
+import time
 
 lcd = LCD.Adafruit_CharLCDPlate()
 
@@ -10,3 +11,13 @@ displaytext = "Hello World"
 
 lcd.clear()
 lcd.message(displaytext)
+
+while (True):
+    if lcd.is_pressed(LCD.UP):
+        lcd.set_backlight(1)
+        lcd.message(displaytext)
+
+    else:
+        lcd.set_backlight(0)
+        lcd.clear()
+        time.sleep(0.5)
